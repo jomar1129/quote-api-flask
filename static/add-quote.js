4a56d789ec2293f66b36775fb7912a4c351325e9
jomar1129 <jomarcotejo@gmail.com>
2021-09-25 09:09:02 +1000

    fix got to homepage route'

--- static/add-quote.js
+++ static/add-quote.js
@@ -16,7 +16,7 @@ submitButton.addEventListener("click", () => {
     <h3>Congrats, your quote was added!</h3>
     <div class="quote-text">${quote}</div>
     <div class="attribution">- ${person}</div>
-    <p>Go to the <a href="index.html">home page</a> to request and view all quotes.</p>
+    <p>Go to the <a href="/">home page</a> to request and view all quotes.</p>
     `;
       newQuoteContainer.appendChild(newQuote);
     });
